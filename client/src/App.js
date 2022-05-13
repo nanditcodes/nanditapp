78947a22a55fdb712c54d607d427fb9931443a62
Nandit Khandelwal <nanidittmali@gmail.com>
2022-05-12 20:52:55 -0400

    undefined error

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import React from "react";
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Axios from 'axios'
 
 function App() {
-  const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  const [userName, setUsername]= useState("");
+  const [passWord, setPassword]= useState("");
+  const  submituserpass = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      username: userName, 
+      password: passWord,
+    }).then(()=> {
+      alert("successful insert");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         {!data ? "Loading...":data}
-        </p>
-      </header>
+    <h1>Login Portal</h1>
+      <div className="form">
+        <label>Username</label>
+        <input type="text" name="Username" onChange={(e)=>{
+        setUsername(e.target.value);
+        }}/>
+        <label>Password</label>
+        <input type="text" name="Password" onChange={(e)=>{
+        setPassword(e.target.value);
+        }} /> 
+        <button onClick={submituserpass}>Submit</button>
+    </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
-const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from nanditapp!"});
+const mysql = require("mysql");
+const SqlString = require("mysql/lib/protocol/SqlString");
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password:"password",
+  database: "AccountInfoDB", 
 });
-app.get('*', (req, res)=>{
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/api/insert", (req, res) => {
+  const UserName = req.body.username
+  const PassWord = req.body.password
+
+  const sqlInsert = "INSERT INTO Acc_Info (username, password) VALUES (?, ?)"
+  db.query(sqlInsert, [UserName, PassWord], (err, result)=> {
+    console.log(result);
+  });
+  
 });
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
