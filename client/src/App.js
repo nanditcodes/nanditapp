202d7b765975aeee48464dce7c7b574315b2061a
Nandit Khandelwal <nanidittmali@gmail.com>
2022-05-18 23:58:01 -0400

    forget/update password errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,76 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Axios from 'axios'
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var flash = require('express-flash');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var indexRouter = require('./routes/index.js');
+var usersRouter = require('./routes/App.js');
+
 
 function App() {
 
-  const [userName, setUsername]= useState("");
-  const [passWord, setPassword]= useState("");
+  const [ user, setUser] = useState({
+    Username:"",
+    Password:""
+})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.use(session({ 
+    secret: '123458cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+ 
+app.use(flash());
+ 
+
+app.use('/', usersRouter);
+ 
+
+app.use(function(req, res, next) {
+  next(createError(404));
+ 
+});
+ 
+
+app.use(function(err, req, res, next) {
+  
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {}; 
+  res.status(err.status || 500);
+  res.render('error');
+   
+});
+ 
+app.listen(4000, function () {
+    console.log('Node app is running on port 4000');
+});
+const handleChange = e => {
+    const { name, value } = e.target
+    setUser({
+        ...user,
+        [name]: value
+    });
+}
+  
   const  submituserpass = () => {
-    Axios.post("http://localhost:3001/api/insert", {
-      username: userName, 
-      password: passWord,
-    }).then(()=> {
+	const { Username, Password} = user
+    Axios.post("http://localhost:3001/api/insert",user)
+	.then(()=> {
       alert("successful insert");
     });
   };
@@ -20,17 +80,15 @@ function App() {
     <h1>Login Portal</h1>
       <div className="form">
         <label>Username</label>
-        <input type="text" name="Username" onChange={(e)=>{
-        setUsername(e.target.value);
-        }}/>
+        <input type="text" name="Username" onChange={ handleChange } />
         <label>Password</label>
-        <input type="text" name="Password" onChange={(e)=>{
-        setPassword(e.target.value);
-        }} /> 
+        <input type="text" name="Password" onChange={ handleChange } /> 
         <button onClick={submituserpass}>Submit</button>
+        <button>Forgot Password</button>
+        <button>Update Password</button>
+        
     </div>
     </div>
   );
 }
-
 export default App;
--- server/index.js
+++ server/index.js
@@ -16,11 +16,16 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/api/insert", (req, res) => {
-  const UserName = req.body.username
-  const PassWord = req.body.password
-
-  const sqlInsert = "INSERT INTO Acc_Info (username, password) VALUES (?, ?)"
+  let UserName = req.body.username
+  let PassWord = req.body.password
+  console.log(req.body);
+  let sqlInsert = "INSERT INTO Acc_Info (username, password) VALUES (?, ?)";
   db.query(sqlInsert, [UserName, PassWord], (err, result)=> {
+    if(err){
+      console.log('error:', err);
+    } else {
+      console.log('result:', result);
+    }
     console.log(result);
   });
   
